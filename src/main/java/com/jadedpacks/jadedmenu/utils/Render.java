e6ee8ed877027f7119b8281e0f94bd0c587e39c9
Kris <cblair91@gmail.com>
2019-02-24 22:36:19 +0000

    dirty code was dirty.

--- src/main/java/com/jadedpacks/jadedmenu/utils/Render.java
+++ src/main/java/com/jadedpacks/jadedmenu/utils/Render.java
@@ -15,25 +15,23 @@ public class Render {
 	}
 
 	public static void drawPartialImage(final int posX, final int posY, final int imageX, final int imageY, final int width, final int height, final int imagePartWidth, final int imagePartHeight) {
-		final double imageWidth = GL11.glGetTexLevelParameteri(3553, 0, 4096);
-		final double imageHeight = GL11.glGetTexLevelParameteri(3553, 0, 4097);
-		final double einsTeilerWidth = 1.0 / imageWidth;
-		final double uvWidth = einsTeilerWidth * imagePartWidth;
-		final double uvX = einsTeilerWidth * imageX;
-		final double einsTeilerHeight = 1.0 / imageHeight;
-		final double uvHeight = einsTeilerHeight * imagePartHeight;
-		final double uvY = einsTeilerHeight * imageY;
+		final double einsTeilerWidth = 1.0 / GL11.glGetTexLevelParameteri(3553, 0, 4096),
+			uvWidth = einsTeilerWidth * imagePartWidth,
+			uvX = einsTeilerWidth * imageX,
+			einsTeilerHeight = 1.0 / GL11.glGetTexLevelParameteri(3553, 0, 4097),
+			uvHeight = einsTeilerHeight * imagePartHeight,
+			uvY = einsTeilerHeight * imageY;
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float)posX, (float)posY, 0.0f);
+		GL11.glTranslatef((float) posX, (float) posY, 0.0f);
 		GL11.glBegin(7);
 		GL11.glTexCoord2d(uvX, uvY);
 		GL11.glVertex3f(0.0f, 0.0f, 0.0f);
 		GL11.glTexCoord2d(uvX, uvY + uvHeight);
-		GL11.glVertex3f(0.0f, (float)height, 0.0f);
+		GL11.glVertex3f(0.0f, (float) height, 0.0f);
 		GL11.glTexCoord2d(uvX + uvWidth, uvY + uvHeight);
-		GL11.glVertex3f((float)width, (float)height, 0.0f);
+		GL11.glVertex3f((float) width, (float) height, 0.0f);
 		GL11.glTexCoord2d(uvX + uvWidth, uvY);
-		GL11.glVertex3f((float)width, 0.0f, 0.0f);
+		GL11.glVertex3f((float) width, 0.0f, 0.0f);
 		GL11.glEnd();
 		GL11.glPopMatrix();
 	}
